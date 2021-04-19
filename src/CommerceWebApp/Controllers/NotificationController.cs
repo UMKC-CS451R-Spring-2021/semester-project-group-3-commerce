e6a0ba291df0683f755fa4b71a2b6a9f4d83a5cc
Benaiah Kilen <benaiahkilen@gmail.com>
2021-04-19 14:58:59 -0500

    Added checks to ensure secure notification_rule insertions

--- src/CommerceWebApp/Controllers/NotificationController.cs
+++ src/CommerceWebApp/Controllers/NotificationController.cs
@@ -5,6 +5,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Security.Claims;
 
 using Commerce_WebApp.Models;
 using Commerce_WebApp.Data;
@@ -27,22 +30,49 @@ namespace Commerce_WebApp.Controllers
 
         public IActionResult Create()
         {
-            ViewBag.Conditions = new List<string>() { "Below", "Above" };
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Customer_Id,Type,Condition,Value")] Notification_Rule notification_Rule)
+        public async Task<IActionResult> Create(Notification_Rule notification_Rule)
         {
-            if (ModelState.IsValid)
+            notification_Rule.Customer_Id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if ((ModelState.IsValid) &&
+                (notification_Rule.Type == "Login" || notification_Rule.Type == "Balance" || notification_Rule.Type == "Deposit" || notification_Rule.Type == "Withdrawal") &&
+                (notification_Rule.Condition == "Above" || notification_Rule.Condition == "Below" || notification_Rule.Condition == "NA") &&
+                (notification_Rule.Value >= 0))
             {
+                /*SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "AddNotificationRule";
+                cmd.Parameters.AddWithValue("@customer_id", User.FindFirstValue(ClaimTypes.NameIdentifier));
+                cmd.Parameters.AddWithValue("@type", notification_Rule.Type);
+                cmd.Parameters.AddWithValue("@condition", notification_Rule.Condition);
+                cmd.Parameters.AddWithValue("@value", notification_Rule.Value);
+                cmd.Parameters.AddWithValue("@notify_text", notification_Rule.Notify_Text);
+                cmd.Parameters.AddWithValue("@notify_email", notification_Rule.Notify_Email);
+                cmd.Parameters.AddWithValue("@notify_web", notification_Rule.Notify_Web);
+                cmd.Parameters.AddWithValue("@message", notification_Rule.Message);*/
+
+                if (notification_Rule.Type == "Login")
+                {
+                    notification_Rule.Condition = "NA";
+                    notification_Rule.Value = 0;
+                }
+
+                if ((notification_Rule.Type == "Deposit" || notification_Rule.Type == "Withdrawal") &&
+                    (notification_Rule.Value <= 0))
+                {
+                    return View(notification_Rule);
+                }
+
                 _context.Add(notification_Rule);
                 await _context.SaveChangesAsync();
                 return RedirectToAction("Manage");
             }
 
-            ViewBag.Conditions = new List<string>() { "Below", "Above" };
             return View(notification_Rule);
         }
 
